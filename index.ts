3c102bfaaab5f7c15bd6dac02d4195e9be3d93ba
zafkiel45 <zafkielc@gmail.com>
2025-05-24 10:19:06 -0300

    refatore(index): moved logic to functions

--- index.ts
+++ index.ts
@@ -4,55 +4,61 @@ import { yellowTextColor, redTextColor } from "./utils/paint_texts";
 
 const ARGS = process.argv.slice(2);
 
-if(ARGS[0] === 'event') {
-  for(const EVENT of EVENT_TYPES) {console.log(EVENT)};
+function showEvents() {
+  for (const EVENT of EVENT_TYPES) {
+    console.log(EVENT);
+  }
   process.exit(0);
-};
+}
 
-if(!ARGS[0]) {
+async function getActivity() {
+  if (!ARGS[0]) {
+    throw new Error(`${redTextColor("[ERROR]")}: You forgot to pass a github user. Please, check the` + "information and try again.");
+  }
+
   try {
-  throw new Error(
-    `${redTextColor('[ERROR]')}: You forgot to pass a github user. Please, check the` +
-    "information and try again.");
-  } catch(err) {
+    // It's possible to get the ETag and store in cache. Posterior requests will
+    // Be compared by ETag, making the request only if the current ETag is different
+    // From old ETag.
+    const EVENT_REQUEST = await fetch(`https://api.github.com/users/${ARGS[0]}/events`);
+    const EVENTS: ApiResponse[] = await EVENT_REQUEST.json();
 
-    if(err instanceof Error) {
+    if (ARGS[1]) {
+      const FILTER_EVENTS = EVENTS.filter((item) => {
+        return item.type === ARGS[1];
+      });
+
+      if (FILTER_EVENTS.length === 0) {
+        console.log(`There are not events for ${yellowTextColor(ARGS[1])}`);
+        process.exit(0);
+      }
+
+      for (const EVENT of FILTER_EVENTS) {
+        console.log(
+          `${ARGS[0]}  perfomed a ${yellowTextColor(EVENT.type)} on ${
+            EVENT.repo.name
+          }`
+        );
+      }
+    } else {
+      for (const EVENT of EVENTS) {
+        console.log(
+          `${ARGS[0]} perfomed a ${yellowTextColor(EVENT.type)}  on ${
+            EVENT.repo.name
+          }`
+        );
+      }
+      process.exit(0);
+    }
+  } catch (err) {
+    if (err instanceof Error) {
       console.error(err.message);
     } else {
-      console.error('Unknown Error');
-    };
-  }
-};
-
-try {
-  // It's possible to get the ETag and store in cache. Posterior requests will 
-  // Be compared by ETag, making the request only if the current ETag is different
-  // From old ETag. 
-  const EVENT_REQUEST = await fetch(`https://api.github.com/users/${ARGS[0]}/events`);
-  const EVENTS: ApiResponse[] = await EVENT_REQUEST.json();
-
-  if(ARGS[1]) {
-    const FILTER_EVENTS = EVENTS.filter((item) => {
-      return item.type === ARGS[1];
-    });
-  
-    if(FILTER_EVENTS.length === 0) {
-      console.log(`There are not events for ${yellowTextColor(ARGS[1])}`);
-      process.exit(0);
-    };
-  
-    for(const EVENT of FILTER_EVENTS) {
-      console.log(`${ARGS[0]}  perfomed a ${yellowTextColor(EVENT.type)} on ${EVENT.repo.name}`);
-    };
-  
-  } else {
-    for(const EVENT  of EVENTS) {
-      console.log(`${ARGS[0]} perfomed a ${yellowTextColor(EVENT.type)}  on ${EVENT.repo.name}`);
+      console.error("Unknown Error");
     }
-    process.exit(0);
-  };
+  }
+}
 
-} catch (err) {
-  console.error(err);
-};
+if (ARGS[0] === "event") showEvents();
 
+getActivity();
